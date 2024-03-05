3b56d6cc6ed73b35d42098d171685de6eae778cf
Yan Moura <emmetoellder@gmail.com>
2024-03-04 21:46:50 -0300

    Lesson 4 and some shenanigans

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,16 +1,45 @@
 import { Component } from '@angular/core';
+import { HousingLocationComponent } from '../housing-location/housing-location.component';
+import { HousingLocation } from '../housinglocation';
+import { NormsComponent } from '../norms/norms.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
-  template: `
-    <p>
-      home works!
-    </p>
-  `,
+  imports: [
+    HousingLocationComponent,
+    NormsComponent
+  ],
+  templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
 
+  counter: number = 0;
+  penis(printSomethingElse: number) {
+    this.counter++;
+    console.log("test: " + this.counter);
+    console.log(printSomethingElse);
+  }
+
+  readonly baseUrl = 'https://angular.io/assets/images/tutorials/faa';
+
+  houses: HousingLocation[] = [];
+
+  housingLocation: HousingLocation = {
+    id: 9999,
+    name: 'Test Home',
+    city: 'Test city',
+    state: 'ST',
+    photo: `${this.baseUrl}/example-house.jpg`,
+    availableUnits: 99,
+    wifi: true,
+    laundry: false,
+  };
+
+  ngOnInit(): void {
+    console.log(this.housingLocation);
+    this.houses.push(this.housingLocation);
+    console.log(this.houses)
+  }
 }
---
+++ src/app/norms/norms.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-norms',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      norms works!
+    </p>
+  `,
+  styleUrl: './norms.component.css'
+})
+export class NormsComponent {
+  // interface Norm {
+  //   orgAcronym: string;
+  //   orgFull: string;
+  //   orgCode: number;
+  //   normDateISO: string;
+  // }
+}
