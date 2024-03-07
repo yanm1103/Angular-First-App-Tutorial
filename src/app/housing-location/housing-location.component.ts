1b72e9d094c3f1b07427a0d7cf55028614ae6c54
Yan Moura <emmetoellder@gmail.com>
2024-03-07 00:49:28 -0300

    Lesson 9 AND Material design added

--- src/app/housing-location/housing-location.component.ts
+++ src/app/housing-location/housing-location.component.ts
@@ -1,16 +1,11 @@
 import { Component, Input } from '@angular/core';
 import { HousingLocation } from '../housinglocation';
+import {MatButtonModule} from '@angular/material/button';
 @Component({
   selector: 'app-housing-location',
   standalone: true,
-  imports: [],
-  template: `
-  <section class="listing">
-    <img class="listing-photo" [src]="housingLocation.photo" alt="Exterior photo of {{housingLocation.name}}">
-    <h2 class="listing-heading">{{ housingLocation.name }}</h2>
-    <p class="listing-location">{{ housingLocation.city}}, {{housingLocation.state }}</p>
-  </section>
-  `,
+  imports: [MatButtonModule],
+  templateUrl: './housing-location.component.html',
   styleUrl: './housing-location.component.css'
 })
 export class HousingLocationComponent {
--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,8 @@
 */
 import { bootstrapApplication,provideProtractorTestingSupport } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 bootstrapApplication(AppComponent,
-    {providers: [provideProtractorTestingSupport()]})
+    {providers: [provideProtractorTestingSupport(), provideAnimationsAsync()]})
   .catch(err => console.error(err));
